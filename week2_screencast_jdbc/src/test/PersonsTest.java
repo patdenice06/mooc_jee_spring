f24975e78877f8f1d2deb662b46fc339e4185d00
Patrick GIRAUD <patrickgiraud@hotmail.com>
2018-05-06 21:30:54 +0200

    Change bean name to Person

--- week2_screencast_jdbc/src/test/PersonsTest.java
+++ week2_screencast_jdbc/src/test/PersonsTest.java
@@ -1,44 +1,56 @@
 package test;
+import java.io.InputStream;
 import java.util.List;
 
-import dao.UserDao;
-import dao.UserDaoMySQLImpl;
-import model.User;
+import dao.DAOConfigurationException;
+import dao.DAOFactory;
+import dao.PersonsDao;
+import model.Persons;
 
-public class UserTest {
-	
-	public static void main(String[] args) {
+public class PersonsTest {
 		
-		UserDao dao = new UserDaoMySQLImpl();
+	public static void main(String[] args) {
+		final String PROPERTIES_FILE = "WEB-INF/dao.properties";
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream propertiesFile = classLoader.getResourceAsStream( PROPERTIES_FILE );
+				
+		if ( propertiesFile == null ) {
+			throw new DAOConfigurationException( "Properties file: " + PROPERTIES_FILE + " not found." );
+		}
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		PersonsDao personsDao = daoFactory.getPersonsDao();
+		
 		
 		// Create new User
-		User newUser = new User();
-		newUser.setEmail("clint@eastwood.com");
-		String password = "clint123";
-		newUser.setFirstName("Clint");
-		newUser.setLastName("EASTWOOD");
-//		try {
-//			newUser.setBirthday( new SimpleDateFormat("yyyy-MM-dd").parse("1930-05-31") );	// Sat May 31 00:00:00 CEST 1930	
-//		} catch (ParseException e) {
-//			throw new Error("Failed to parse birthday. " + e);
-//		}
-		dao.create(newUser, password);
+//		Persons newUser = new Persons();
+//		newUser.setEmail("east@eastwood.com");
+//		String password = "clint123";
+//		newUser.setFirstName("Clint");
+//		newUser.setLastName("EASTWOOD");
+////		try {
+////			newUser.setBirthday( new SimpleDateFormat("yyyy-MM-dd").parse("1930-05-31") );	// Sat May 31 00:00:00 CEST 1930	
+////		} catch (ParseException e) {
+////			throw new Error("Failed to parse birthday. " + e);
+////		}
+//		personsDao.create(newUser, password);
 		
 		// List all users
-		List<User> users = dao.listAll();
-		if ( users.isEmpty() ) {
+		List<Persons> personsList = personsDao.listAll();
+		if ( personsList.isEmpty() ) {
 			System.out.println("Aucun utilisateur en base");
 		}
 		else
-		for( User user : users ) {
+		for( Persons person : personsList ) {
 			System.out.println( 
 				String.format(
 					"%40s | %16s | %16s	| %20s	| %3d",
-					user.getEmail(),
-					user.getFirstName(),
-					user.getLastName(),
-					user.getBirthday(),
-					user.getAge()
+					person.getEmail(),
+					person.getFirstName(),
+					person.getLastName(),
+					person.getBirthday(),
+					person.getAge()
 				)
 			);
 		}
