4194b3d6a76c77bdbb07791d8c8f196933d36fb8
Patrick GIRAUD <patrickgiraud@hotmail.com>
2018-05-06 21:03:56 +0200

    Instanciate once the DAOFactory for the scope of the web app

--- week2_screencast_jdbc/src/config/InitDaoFactory.java
+++ week2_screencast_jdbc/src/config/InitDaoFactory.java
@@ -7,30 +7,31 @@ import javax.servlet.ServletContextListener;
 import dao.DAOFactory;
 
 /**
- * Instanciate once the DAOFactory for the scope of that web app
+ * Instanciate once the DAOFactory for the scope of the web app
  */
 public class InitDaoFactory implements ServletContextListener{
 
 	private static final String ATT_DAO_FACTORY = "daofactory";
 	private DAOFactory daoFactory;
 	
+
 	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		// Get the ServletContext when that web is loading
+	public void contextInitialized(ServletContextEvent event) {
+		// Get the ServletContext when the web is loading
 		ServletContext servletContext = event.getServletContext();
 		
 		// Instanciate the DAOFactory
 		this.daoFactory = DAOFactory.getInstance();
 		
-		// Set this attribute for the scope of that web app with read only access
+		// Set this attribute for the scope of the web app with read only access
 		servletContext.setAttribute(ATT_DAO_FACTORY, this.daoFactory);
-	}
-
-	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		// Nothing to do when closing that web app
+		
 	}
 	
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Nothing to do when closing the web app
+	}
 	
 
 }
