1f8cbbcd5a664591f52c2853dd8e30a1b2ea3550
Patrick GIRAUD <patrickgiraud@hotmail.com>
2018-05-06 21:25:17 +0200

    Registration Person bean delegates to RegistrationForm object

--- week2_screencast_jdbc/src/servlets/Registration.java
+++ week2_screencast_jdbc/src/servlets/Registration.java
@@ -1,68 +1,73 @@
-package controller;
+package servlets;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.UserDao;
-import dao.UserDaoMySQLImpl;
-import model.User;
+import dao.PersonsDao;
+import forms.RegsitrationForm;
+import model.Persons;
+import dao.DAOFactory;
 
 /**
  * User registration servlet
  */
 @WebServlet("/registration")
-public class RegistrationServlet extends HttpServlet {
+public class Registration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	UserDao dao;
+	
+//	public static final String CONF_DAO_FACTORY = "daofactory";	// not config.initDaoFactory.ATT_DAO_FACTORY ???
+	public static final String ATT_DAO_FACTORY = "daofactory";	
+	public static final String ATT_USER = "user";
+	public static final String ATT_FORM	= "form";
+	public static final String VUE = "/WEB-INF/registration.jsp";
+	private PersonsDao personsDao;	
        
-    public RegistrationServlet() {
+    public Registration() {
         super();
     }
 
 	@Override
 	public void init() throws ServletException {
 		System.out.println("UserServlet.init()");
-		dao = new UserDaoMySQLImpl();
+		/* Get instance of our DAO person */
+//		this.personsDao = ( (DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY) ).getPersonsDao() ;
+		this.personsDao = ( (DAOFactory) getServletContext().getAttribute(ATT_DAO_FACTORY) ).getPersonsDao() ;
 	}
     
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("RegistrationServlet.doGet()");
-		request.getRequestDispatcher("/WEB-INF/registration.jsp").forward(request, response);		
+		/* Dispaly registration page */
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );		
 		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {			
 		// récupérer les paramètres saisis dans le formulaire de registration
-		String email = (String) request.getParameter("inputEmail");
-		String password = (String) request.getParameter("inputPassword");
-		String firstname = (String) request.getParameter("inputFirstname");
-		String lastname = (String) request.getParameter("inputLastname");
-		String  birthday = (String) request.getParameter("inputBirthday");
 		
-		System.out.println("email : " + email);
-		System.out.println("password : " + password);
-		System.out.println("firstname : " + firstname);
-		System.out.println("lastname : " + lastname);
-		System.out.println("birthday : " + birthday);
+		/* Prépare registration object */
+		RegsitrationForm form = new RegsitrationForm( personsDao );
 		
-		// create this new User
-		User user = new User();
-		user.setEmail(email);
-		user.setFirstName(firstname);
-		user.setLastName(lastname);
-		user.setBirthday( LocalDate.parse(birthday) );
+		/* Use request object to retrieve person bean */
+		Persons person = form.registerPerson( request );
 		
-		// record this User in table users.persons
-		dao.create(user, password);
+		/* Set form and bean in request object */
+		request.setAttribute( ATT_FORM, form );
+		request.setAttribute( ATT_USER, person );		
+		
+		
+//		String email = (String) request.getParameter("inputEmail");
+//		String password = (String) request.getParameter("inputPassword");
+//		String firstname = (String) request.getParameter("inputFirstname");
+//		String lastname = (String) request.getParameter("inputLastname");
+//		String  birthday = (String) request.getParameter("inputBirthday");
 		
 		// show new users list
-		request.setAttribute( "persons", dao.listAll() );
+		request.setAttribute( "persons", personsDao.listAll() );
 		request.getRequestDispatcher("/WEB-INF/user-list.jsp").forward(request, response);		
 
 		
