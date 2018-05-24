5c8649c945af11ebac94d6bcb36ae9bf9c477f8b
Patrick GIRAUD <patrickgiraud@hotmail.com>
2018-05-24 21:44:27 +0200

    Implementation of the logic of fields validation

--- week2_screencast_jdbc/src/forms/RegsitrationForm.java
+++ week2_screencast_jdbc/src/forms/RegsitrationForm.java
@@ -1,9 +1,14 @@
 package forms;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.jasypt.util.password.ConfigurablePasswordEncryptor;
+
+import dao.DAOException;
 import dao.PersonsDao;
 import model.Persons;
 
@@ -12,38 +17,181 @@ import model.Persons;
  */
 public class RegsitrationForm {
 
-	private PersonsDao personsDao;
+	private PersonsDao personsDao = null;
+	private String result = null;	
+	private Map<String, String> errors = new HashMap<String, String>();
+	private static final String DIGEST_ALGO = "SHA-256";		
+	private static final String EMAIL_FIELD = "inputEmail";
+	private static final String PASSWORD_FIELD = "inputPassword";
+	private static final String CONFIRM_PASSWORD_FIELD = "inputConfirmPassword";
+	private static final String FIRSTNAME_FIELD = "inputFirstname";
+	private static final String LASTNAME_FIELD = "inputLastname";
+	private static final String BIRTHDAY_FIELD = "inputBirthday";
 	
 	public RegsitrationForm(PersonsDao personsDao) {
 		this.personsDao = personsDao;
 	}
 
+	
+	/**
+	 * Set an error entry
+	 * @param field	The field of the form that caused the error
+	 * @param message	The error cause
+	 */
+	private void setError(String field, String message) {
+		errors.put(field, message);		
+	}
+
+	/**
+	 * Error getter
+	 * @return Errors map
+	 */
+	public Map<String, String> getErrors() {
+		return errors;
+	}
+
+	/**
+	 * Result getter
+	 * @return	The result
+	 */
+	public String getResult() {
+		return result;
+	}
+
+	public static String getEmailField() {
+		return EMAIL_FIELD;
+	}	
+	
+	
+	
+	/**
+	 * Set Person bean fields from registration form
+	 * @param request
+	 * @return Person bean
+	 * @throws FormValidationException 
+	 */
 	public Persons registerPerson(HttpServletRequest request) {
 		System.out.println("RegsitrationForm.registerPerson()");
-		String password = (String) request.getParameter("inputPassword");
 		
-		Persons person = new Persons();
-		person.setEmail( (String) request.getParameter("inputEmail") );
-		person.setFirstName( (String) request.getParameter("inputFirstname") );
-		person.setLastName( (String) request.getParameter("inputLastname") );
-		person.setBirthday( LocalDate.parse( (String) request.getParameter("inputBirthday") ) );
-		
+		// Get registration form input parameters 
+		String email = ( (String) request.getParameter( EMAIL_FIELD ) );		
+		String password = (String) request.getParameter( PASSWORD_FIELD );
+		String confirmPassword = (String) request.getParameter( CONFIRM_PASSWORD_FIELD );
+		String firstName = (String) request.getParameter( FIRSTNAME_FIELD );
+		String lastName = (String) request.getParameter( LASTNAME_FIELD );
+		LocalDate birthday = LocalDate.parse( (String) request.getParameter( BIRTHDAY_FIELD ) );
+
 		// DEBUG
-		System.out.println("email : " + person.getEmail());
-		System.out.println("password : " + password);
-		System.out.println("firstname : " + person.getFirstName());
-		System.out.println("lastname : " + person.getLastName());
-		System.out.println("birthday : " + person.getBirthday().toString());
-		
-		// TODO If email already exists then set an error, else create the new person
-		if( personsDao.find( person.getEmail() ) != null ) {
+		System.out.println( EMAIL_FIELD +" = "+  email  );
+		System.out.println( PASSWORD_FIELD +" = "+ password);
+		System.out.println( CONFIRM_PASSWORD_FIELD +" = "+ confirmPassword);
+		System.out.println( FIRSTNAME_FIELD +" = "+  firstName );
+		System.out.println( LASTNAME_FIELD +"  "+  lastName );
+		System.out.println( BIRTHDAY_FIELD +" = "+ birthday.toString() );
 			
-		}
-		else {
-			personsDao.create(person, password);			
-		}
+		Persons person = new Persons();
+		
+		try {
+			
+			checkEmail(email, person);
+			checkPasswords(password, confirmPassword, person);
+
+			if( errors.isEmpty() ) {
+				// Password, confirm password and email validated and set to to person bean
+				person.setFirstName(firstName);
+				person.setLastName(lastName);
+				person.setBirthday(birthday);				
+				personsDao.create(person);
+				result = "Registration success.";
+			}
+			else {
+				result = "Registration failure.";
+			}
+						
+		} catch (DAOException e) {
+			result = "Registration failure: An error has occurred, thanks to register again.";
+			e.printStackTrace();
+		}	
 		
 		return person;
+	}
+
+	
+	/**
+	 * Call email validation, and init email bean property
+	 * @param email
+	 * @param person
+	 */
+	private void checkEmail(String email, Persons person) {
+		try {
+			validateEmail(email);
+		} catch (FormValidationException e) {
+			setError(EMAIL_FIELD, e.getMessage());
+			System.out.println( getErrors().get(EMAIL_FIELD) );
+		}
+		person.setEmail(email);
+	}
+
+	
+	/**
+	 * Validate email if it is unique
+	 * @param email
+	 * @throws FormValidationException
+	 */
+	private void validateEmail(String email) throws FormValidationException{
+		if( personsDao.find(email) != null ) { 
+			System.out.println("email already exists ==> set an error");
+			throw new FormValidationException("Email already exists. Thanks to choose another email.");
+		}
+	}
+
+	
+	/**
+	 * Call validate passwords method, encrypt passwords and set bean property "cryptedPassword"
+	 * @param password
+	 * @param confirmPassword
+	 * @param person 
+	 * @throws FormValidationException
+	 */
+	private void checkPasswords(String password, String confirmPassword, Persons person) {
+			try {
+				validatePasswords(password, confirmPassword);
+			} catch (FormValidationException e) {
+				if( !e.getMessage().contains("Confirm") ) {
+					setError(PASSWORD_FIELD, e.getMessage());
+					setError(CONFIRM_PASSWORD_FIELD, null);					
+				}
+				else {
+					setError(PASSWORD_FIELD, null);
+					setError(CONFIRM_PASSWORD_FIELD, e.getMessage());										
+				}					
+			}
+			
+			// Passwords are validated, so encrypt password
+			ConfigurablePasswordEncryptor passwordEncryptor = new ConfigurablePasswordEncryptor();
+			passwordEncryptor.setAlgorithm( DIGEST_ALGO );
+			passwordEncryptor.setPlainDigest( false );
+			String cryptedPassword = passwordEncryptor.encryptPassword( password);
+			
+//			passwordEncryptor.checkPassword(plainPassword, encryptedPassword);
+			
+			person.setCryptedPassword( cryptedPassword );
+	}
+	
+	
+	/**
+	 * Validate password and confirm password
+	 * @param password
+	 * @param confirmPassword
+	 * @throws FormValidationException 
+	 */
+	private void validatePasswords(String password, String confirmPassword) throws FormValidationException{
+		if( !password.equals(confirmPassword) ) {
+			throw new FormValidationException("Confirm password does not match password. Please retry. ");
+		}
+		else if ( password.trim().length() < 6 ) {
+			throw new FormValidationException("Password must contains at least 6 characters long. Pease retry.");
+		}
 	}
 
 	
