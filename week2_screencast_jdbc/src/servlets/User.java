6b32fd25a72809d26b74e66e831eefa9de49a1e0
Patrick GIRAUD <patrickgiraud@hotmail.com>
2018-05-26 20:01:59 +0200

    Fix use of DAO

--- week2_screencast_jdbc/src/servlets/User.java
+++ week2_screencast_jdbc/src/servlets/User.java
@@ -1,6 +1,7 @@
 package servlets;
 import javax.servlet.http.*;
 
+import dao.DAOFactory;
 import dao.PersonsDao;
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -10,13 +11,20 @@ import javax.servlet.annotation.WebServlet;
 public class User extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	PersonsDao dao;
+	public static final String ATT_DAO_FACTORY = "daofactory";		
+	private PersonsDao personsDao;	
 
+	@Override
+	public void init() throws ServletException {
+		System.out.println("UserServlet.init()");
+		/* Get instance of our DAO person */
+		this.personsDao = ( (DAOFactory) getServletContext().getAttribute(ATT_DAO_FACTORY) ).getPersonsDao() ;
+	}	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
 		System.out.println("UserServlet.doGet()");		
-		req.setAttribute( "persons", dao.listAll() );
+		req.setAttribute( "persons", personsDao.listAll() );
 		req.getRequestDispatcher("/WEB-INF/user-list.jsp").forward(req, resp);		
 		
 	}
