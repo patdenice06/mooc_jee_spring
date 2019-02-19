4ca255c0bfea87d49887436d93b2f35eae587e00
Patrick GIRAUD <patrickgiraud@hotmail.com>
2019-02-19 17:36:25 +0100

    git add week3/screencast/sc10/src/main/java/jpa_queries_jpql/AppJpql.java

--- week3/screencast/sc10/src/main/java/App.java
+++ week3/screencast/sc10/src/main/java/App.java
@@ -3,11 +3,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import data.AirlineCoverage;
 import flights.Airline;
 
 public class App {
@@ -40,22 +39,58 @@ public class App {
 		
 		showAirlines(airlines);
 		
-		// Another JPA code format to execute the same request
-//		TypedQuery<Airline> query= em.createQuery(
-//				" SELECT DISTINCT rt.airline FROM Route AS rt "
-//				+" JOIN rt.airline AS al "
-//				+" JOIN rt.destination AS dst "
-//				+" WHERE al.country = :country AND dst.city = :city", Airline.class);
-//		query.setParameter("country", "France");
-//		query.setParameter("city", "Miami");
-//		airlines = query.getResultList();
-//				
-//		showAirlines(airlines);
 				
 		
 		log.debug("Search Number of airlines from CDG");
+		Long count = em.createQuery(
+				"select count(distinct rt.airline) "
+				+"from Route as rt "
+				+"join rt.source as ap "
+				+"where ap.iata = :code ",
+				Long.class
+		)
+		.setParameter("code", "CDG")
+		.getSingleResult();
+		System.out.println(count + " airlines start from CDG");
+		
 		
 		log.debug("Search Best airlines with number of dest airports to USA from CDG, ordered by 'coverage'");
+		List<AirlineCoverage> airlineCoverages = em.createQuery(
+				"Select new data.AirlineCoverage( count(rt.destination), apD.country, al.name ) "
+			   +"from Route as rt "
+			   +"join rt.destination as apD "
+			   +"join rt.source as apS "
+			   +"join rt.airline as al "
+			   +"where apD.country = :country and apS.iata = :code "
+			   +"Group by apD.country, al.name "	// On doit placer les colonnes du "Select" qui ne sont pas des fonctions d'agrégation  
+			   +"Order by count(rt.destination) desc ",
+			   AirlineCoverage.class
+		)
+		.setParameter("country", "United States")
+		.setParameter("code", "CDG")
+		.getResultList();
+		
+		System.out.println( "Best airlines with number of dest airports to USA from CDG, ordered by 'coverage'" );		
+		System.out.println(
+				String.format("%3s	|	%-20s	|	%-30s",
+						"Routes",
+						"Country",
+						"Airline"
+				)
+			);
+		
+		for (AirlineCoverage airlineCoverage : airlineCoverages) {
+			System.out.println(
+					String.format("%3d	|	%-20s	|	%-30s",
+							airlineCoverage.getNbRoutes(),
+							airlineCoverage.getCountry(),
+							airlineCoverage.getAirlineName()
+					)
+				);
+		}
+		
+		
+		
 		
 		log.debug("Close Entity Manager");
 		em.close();
