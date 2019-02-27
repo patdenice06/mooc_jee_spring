27a3a023b9a7d2b7c43cb657e8b6a330398f78e6
Patrick GIRAUD <patrickgiraud@hotmail.com>
2019-02-27 20:15:04 +0100

    Update logger argument

--- week3/screencast/sc10/src/main/java/examples/CompteApp.java
+++ week3/screencast/sc10/src/main/java/examples/CompteApp.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class CompteApp {
 
-	private static Logger log = LoggerFactory.getLogger( Compte.class );
+	private static Logger log = LoggerFactory.getLogger( CompteApp.class );
 	private static EntityManagerFactory emf = null;
 	private static EntityManager em = null;
 	
