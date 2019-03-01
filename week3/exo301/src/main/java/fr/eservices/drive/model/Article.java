578d7a032be34f4e74a467b30b56d43775be943c
Patrick GIRAUD <patrickgiraud@hotmail.com>
2019-03-01 15:01:39 +0100

    Create model attributes and JPA annotations

--- week3/exo301/src/main/java/fr/eservices/drive/model/Article.java
+++ week3/exo301/src/main/java/fr/eservices/drive/model/Article.java
@@ -1,5 +1,93 @@
 package fr.eservices.drive.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Article {
 
+	@Id
+	/**
+	 * Article ID 	Unique identifier for this article.
+	 */
+	private int id;
+	
+	/**
+	 * Barcode coded with 13 characters  
+	 */
+	private String ean;
+	
+	/**
+	 * Price is in cents representing
+	 */
+	private int price;
+	
+	/**
+	 * VAT number (0.20 = 20%)
+	 */
+	private double vat;
+	
+	/**
+	 * Name of the article
+	 */
+	private String name;
+	
+	/**
+	 * Articles may contain many categories
+	 */
+	@ManyToMany
+	private List<Category> categories;
+
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEan() {
+		return ean;
+	}
+
+	public void setEan(String ean) {
+		this.ean = ean;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public double getVat() {
+		return vat;
+	}
+
+	public void setVat(double vat) {
+		this.vat = vat;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	
 }
--- week3/exo301/src/main/java/fr/eservices/drive/model/Category.java
+++ week3/exo301/src/main/java/fr/eservices/drive/model/Category.java
@@ -1,5 +1,50 @@
 package fr.eservices.drive.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+	/**
+	 * Category ID 	Unique identifier for this category.
+	 */
+	@Id
+	private int id;
+	
+	/**
+	 * Name of the this category
+	 */
+	private String name;
+	
+	/**
+	 * Order index that will allow to sort products on an order given by the administrator 
+	 */
+	private int orderIdx;
+
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getOrderIdx() {
+		return orderIdx;
+	}
+
+	public void setOrderIdx(int orderIdx) {
+		this.orderIdx = orderIdx;
+	}
+	
 }
