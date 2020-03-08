5b56ecc14ddbc453f9775dbb47888f3de19667ee
Patrick GIRAUD <patrickgiraud@hotmail.com>
2020-03-08 21:29:22 +0100

    Add missing source for HistorySource with DAO JPA

---
+++ week4/exo402/src/main/java/fr/eservices/drive/dao/HistorySourceJpa.java
@@ -0,0 +1,104 @@
+package fr.eservices.drive.dao;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.xml.bind.DatatypeConverter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import fr.eservices.drive.model.StatusHistoryEntity;
+import fr.eservices.drive.model.TimeObject;
+import fr.eservices.drive.web.HistorySource;
+
+@Component
+@Qualifier("jpa")
+public class HistorySourceJpa implements HistorySource {
+	
+	@Autowired
+	private EntityManager em;
+	@Autowired
+	private EntityTransaction tx;
+	
+	private List<StatusHistory> status;
+	
+	
+	public HistorySourceJpa() {
+		status = new ArrayList<StatusHistory>( Arrays.asList(
+			createStatus( "ORDERED",          "2017-11-28T10:00:00Z" ),
+			createStatus( "READY_TO_DELIVER", "2017-11-28T10:00:00Z" )
+		));
+	}
+
+	SimpleDateFormat sdf = new SimpleDateFormat("");
+	private StatusHistory createStatus(String statusName, String dateTime) {
+		StatusHistory status = new StatusHistory();
+		status.setStatus( Status.valueOf(statusName) );
+		status.setStatusDate( DatatypeConverter.parseDate(dateTime).getTime() );
+		TimeObject t = new TimeObject();
+		Date now = new Date();
+		t.day = DateFormat.getDateInstance(DateFormat.SHORT).format(now);
+		t.time = DateFormat.getTimeInstance(DateFormat.SHORT).format(now);
+		t.locale = Locale.getDefault().toString();
+		t.timestamp = (long) (now.getTime() / 1000);
+		status.setTimeObject(t);
+		return status;
+	}
+
+	@Override
+	public List<StatusHistory> orderHistory(int orderId) {
+		return status;
+	}
+	
+	@Override
+	public void addHistoryStatus(int orderId, StatusHistory statusHistory) 
+	throws DataException {
+		if ( orderId == 666 ) throw new DataException("No such order");
+		status.add(statusHistory);
+	}
+	
+	
+	void save(StatusHistoryEntity statusEntity) {
+		try {
+			tx.begin();
+				em.persist( statusEntity );		
+			tx.commit();			
+		} catch (Exception e) {
+			tx.rollback();
+		}		
+	}
+	
+	
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myApp");
+		HistorySourceJpa dao = new HistorySourceJpa();
+		dao.em = emf.createEntityManager();
+		dao.tx = dao.em.getTransaction();				
+		
+		StatusHistoryEntity statusEntity = null;
+		String s = null;
+		Date d = null;
+		for (int i = 0; i < dao.status.size(); i++) {
+			statusEntity = new StatusHistoryEntity();
+			s = dao.status.get(i).getStatus().toString();
+			d = dao.status.get(i).getStatusDate();
+			statusEntity.setStatus(s);
+			statusEntity.setStatusDate(d);
+			dao.save(statusEntity);
+		}
+		
+		emf.close();
+	}
+
+}
